7b5453f6ab7c942279537561123e62425941e128
Evandro Souza <evandroferreiras@gmail.com>
2020-01-18 17:40:49 -0300

    chore: added docker-compose to mongodb and redis

--- machinery/server.go
+++ machinery/server.go
@@ -5,7 +5,6 @@ import (
 	"github.com/RichardKnop/machinery/v1"
 	"github.com/RichardKnop/machinery/v1/config"
 	"github.com/RichardKnop/machinery/v1/tasks"
-	"github.com/RichardKnop/machinery/v1/log"
 	"sync"
 
 )
